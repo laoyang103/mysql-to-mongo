cab2e9516999851f677f5a66a342b51508bdaae0
laoyang103 <553069938@qq.com>
2015-04-30 16:40:00 +0800

    不缓存索引树，降低内存消耗

--- insert.py
+++ insert.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import json
 import time
 import pymongo
 import datetime
@@ -8,7 +7,6 @@ import MySQLdb
 
 _id, client, server, protocol, method, url, httpreturncode, location, referer, useragent, contenttype, _bytes, begintime, endtimewithpayload, flowhash, cookie, terminator, appllatency, clientlatency, serverlatency, rtt, applicationid, application, balancerhost, serverip, rehttppkts, client2server_teid, server2client_teid, flowusername, fbookchat, port, loc_id, loc_id_cn, forward = range(34)
 names = ["_id", "client", "server", "protocol", "method", "url", "httpreturncode", "location", "referer", "useragent", "contenttype", "_bytes", "begintime", "endtimewithpayload", "flowhash", "cookie", "terminator", "appllatency", "clientlatency", "serverlatency", "rtt", "applicationid", "application", "balancerhost", "serverip", "rehttppkts", "client2server_teid", "server2client_teid", "flowusername", "fbookchat", "port", "loc_id", "loc_id_cn", "forward"]
-ips = [3720385042, 3396081437, 1875813415, 2099758888, 2099713289, 1696398356, 2008822372, 3232235788, 2071819107, 2067447099, 3396084028, 2071797212, 2099708298, 3395987888]
 
 conn = pymongo.Connection()
 mongodb = conn.test
@@ -16,8 +14,8 @@ ipset = mongodb.ipset
 dataset = mongodb.dataset
 ipset.drop()
 dataset.drop()
-ipdict = {}
 record_cached = {}
+urlcnt_cached = {}
 
 #sql = "SELECT * FROM http_log_000000004 where server = '192.168.1.12' limit 0, 50"
 mysqldb = MySQLdb.connect("localhost","root","123456","ipm" )
@@ -33,30 +31,39 @@ def insert(sql):
         no_point_domain = row[server].replace('.', '_')
         if -1 != no_point_url.find('?'): no_point_url = no_point_url[:no_point_url.find('?')]
 
-        # build index docment
-        if not ipdict.has_key(row[serverip]):
-            ipdict[row[serverip]] = {}
-            ipdict[row[serverip]]['_id'] = row[serverip]
-        if not ipdict[row[serverip]].has_key(no_point_domain):
-            ipdict[row[serverip]][no_point_domain] = {}
-        if not ipdict[row[serverip]][no_point_domain].has_key(no_point_url):
-            ipdict[row[serverip]][no_point_domain][no_point_url] = {}
+        # build index tree
+        ipdoc = ipset.find_one({"_id": row[serverip]})
+        if not ipdoc: 
+            ipdoc = {"_id": row[serverip]}
+            ipset.insert(ipdoc)
+        if not ipdoc.has_key(no_point_domain): 
+            ipdoc[no_point_domain] = {}
+            ipset.update({"_id": row[serverip]}, ipdoc)
+        if not ipdoc[no_point_domain].has_key(no_point_url):
+            ipdoc[no_point_domain][no_point_url] = {}
+            ipset.update({"_id": row[serverip]}, ipdoc)
+        tree_url = ipdoc[no_point_domain][no_point_url]
 
-        # insert url timestamp 
-        tree_url = ipdict[row[serverip]][no_point_domain][no_point_url]
-        if (not tree_url.has_key(timestamp) and not tree_url.has_key('curr')) or (tree_url[tree_url['curr']]['count'] > 15000): 
-            tree_url[timestamp] = {}
-            tree_url[timestamp]['count'] = 0
-            tree_url[timestamp]['key'] = dataset.insert({'time': timestamp})
-            tree_url['curr'] = timestamp
-        tree_url[tree_url['curr']]['count'] += 1
+        # this url has no data or save more than 15000
+        if not tree_url.has_key('curr') or urlcnt_cached[tree_url['curr']['key']] > 15000:
+            if tree_url.has_key('curr'): del urlcnt_cached[tree_url['curr']['key']]
+            insert_id = dataset.insert({})
+            tree_url['curr'] = {"key": insert_id, "count": 0}
+            tree_url[timestamp] = {"key": insert_id, "count": 0}
+            ipset.update({"_id": row[serverip]}, ipdoc)
+            urlcnt_cached[insert_id] = 0
+
+        # init new count cached from databases and common add 
+        if not urlcnt_cached.has_key(tree_url['curr']['key']):
+            urlcnt_cached[tree_url['curr']['key']] = tree_url['curr']['count']
+        urlcnt_cached[tree_url['curr']['key']] += 1
 
         # fill data docment record
         fields = {}
         for i in range(34): fields[names[i]] = row[i]
 
         # create insert cached
-        insert_id = tree_url[tree_url['curr']]['key']
+        insert_id = tree_url['curr']['key']
         if not record_cached.has_key(insert_id):
             record_cached[insert_id] = []
 
@@ -84,8 +91,8 @@ insert(sql)
 
 
 # write index dict to mongodb
-for i in ipdict:
-    ipset.insert(ipdict[i])
+#for i in ipdict:
+#    ipset.insert(ipdict[i])
 
 #        starttime1 = time.time()  
 #        endtime1 = time.time()  
